b69e8eb334e88e9ebfbe3488aade69d16daff640
Pat  Pignataro <pat6284@msn.com>
2019-05-08 18:04:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much.";
+
+const sensor = (word, string) => {
+  var regEx = new RegExp(word, 'p');
+  return string.replace(regex, 'BLEEP')
+} 
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  // startTicking() {
+  //   setInterval(() => {
+  //     this.time++;
+  //   }, 1000);
+  //},
+};
+
+// const digitalClock = {
+//   time: Math.round(Date.now() / 1000),
+//   startTicking() {
+//     setInterval(() => {
+//       this.time++;
+//     },1000);
+//   },
+// };
+
+// function censor(word, string) {
+//   var regex = new RegExp(word, 'g');
+//   return string.replace(regex, 'BLEEP')
+// }
+
+// const violenceCensor = censor.bind(null, 'violence');
+
+// const drugsCensor = censor.bind(null, 'drugs');
