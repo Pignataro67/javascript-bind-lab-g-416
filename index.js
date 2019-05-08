242578f07a7b546d8eb7c890cfeebba087f4f0eb
Pat  Pignataro <pat6284@msn.com>
2019-05-08 18:05:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = "I don't do much.";
 
-const sensor = (word, string) => {
-  var regEx = new RegExp(word, 'p');
-  return string.replace(regex, 'BLEEP')
-} 
+// const sensor = (word, string) => {
+//   var regEx = new RegExp(word, 'p');
+//   return string.replace(regex, 'BLEEP')
+// } 
 
 const digitalClock = {
   time: Math.round(Date.now() / 1000),
@@ -14,20 +14,11 @@ const digitalClock = {
   },
 };
 
-// const digitalClock = {
-//   time: Math.round(Date.now() / 1000),
-//   startTicking() {
-//     setInterval(() => {
-//       this.time++;
-//     },1000);
-//   },
-// };
+function censor(word, string) {
+  var regex = new RegExp(word, 'g');
+  return string.replace(regex, 'BLEEP')
+}
 
-// function censor(word, string) {
-//   var regex = new RegExp(word, 'g');
-//   return string.replace(regex, 'BLEEP')
-// }
+const violenceCensor = censor.bind(null, 'violence');
 
-// const violenceCensor = censor.bind(null, 'violence');
-
-// const drugsCensor = censor.bind(null, 'drugs');
+const drugsCensor = censor.bind(null, 'drugs');
